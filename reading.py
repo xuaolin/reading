6b005bf41d711712bbb2a383317336e862393e8d
xuaolin <xuaolin007@gmail.com>
2019-03-02 17:12:44 -0600

    add average words for each review

--- reading.py
+++ reading.py
@@ -6,5 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1 # count = count + 1
 		if count % 1000 == 0:
 			print(len(data))
+print('档案读取完毕，总共有', len(data), '个档案')
 
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('the average words for each review is', sum_len/len(data))
 
